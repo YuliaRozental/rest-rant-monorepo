b86155521691033931d20f3cfbf6a7f5fe86fb6c
Yulia <yuyarozental@gmail.com>
2022-11-12 10:54:58 -0500

    using sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -3,7 +3,7 @@ const db = require("../models")
 const bcrypt = require('bcrypt')
 
 const { User } = db
-    
+
 router.post('/', async (req, res) => {
     let user = await User.findOne({
         where: { email: req.body.email }
@@ -14,8 +14,25 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
+
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+       let user = await User.findOne({
+        where: {
+                userId: req.session.userId
+        }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 
 module.exports = router
